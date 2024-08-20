f2c059fb82d2f781d77e4a1e9393a52849500b28
romariolili <turmatango282@gmail.com>
2024-08-20 13:42:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,6 @@ def get_link():
             </div>
         </div>
     ''', chat_history=chat_history)
-
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-
-
