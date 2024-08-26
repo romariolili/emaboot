4f773b77e0b564b24b6b6641f8d53bfe72bd164f
romariolili <turmatango282@gmail.com>
2024-08-26 16:56:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,8 @@ template = '''
         /* Ajustes para versão mobile */
         @media screen and (max-width: 768px) {
             .chat-box {
-                width: 90%; /* Ajusta a largura para 90% da tela em dispositivos móveis */
+                width: 50%; /* Ajusta a largura para 90% da tela em dispositivos móveis */
+                max-width: 300px; /* Limita a largura máxima da caixa em dispositivos móveis */
                 margin-top: 5%; /* Ajuste para manter a caixa dentro da imagem de fundo em telas menores */
                 max-height: 50vh; /* Limita a altura da caixa a 50% da altura da viewport */
             }
