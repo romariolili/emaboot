75180bbf749f0a8f3ed0e71b6e58819a458d4a90
romariolili <turmatango282@gmail.com>
2024-08-26 09:45:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ template = '''
             padding: 20px;
             justify-content: flex-start; /* Alinhamento à esquerda */
             flex-direction: column;
-            margin-top: 180px; /* Reduz a margem superior para subir a caixa */
+            margin-top: 60px; /* Reduz a margem superior para subir a caixa */
         }
         .chat-box {
             width: 50%;
